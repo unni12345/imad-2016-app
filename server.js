6e74adb640e857a09e80620b2a7edf061ab1e30b
unni12345 <rajkrishnanv@gmail.com>
2016-09-21 18:39:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,8 +6,12 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'myprofile.html'));
+});
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
