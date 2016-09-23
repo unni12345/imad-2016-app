8120e63429143eb28db814a9782d13606f6ffec3
unni12345 <rajkrishnanv@gmail.com>
2016-09-23 14:30:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-app.get('/documentone', function (req, res) {
+app.get('/article-one', function (req, res) {
   res.sendFile("document will be updated quickly");
 });
 
