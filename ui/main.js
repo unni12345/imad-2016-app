e9319c3d34c0908fb035c3c3bad7fab8dc4e0ffc
unni12345 <rajkrishnanv@gmail.com>
2016-11-02 18:40:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,11 +7,12 @@ submit.onclick= function() {
     //make a request to the server
     //capture a list andrender it as a list
     //From now is shit
-    //for(  i=0; i< names.length();i=i+1){
-      //  list='<li>'+names[i]+'</li>';
+    for(  i=0; i< names.length();i=i+1){
+        list='<li>'+names[i]+'</li>';
         alert("something");
+    }
     
     
-//    var ul=document.getElememtById("commentList");
-   // ul.innerHTML=list;
+   var ul=document.getElememtById("commentList");
+   ul.innerHTML=list;
 };
