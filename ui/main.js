aa8a217f293c22cead69ec9126c716262b69863a
unni12345 <rajkrishnanv@gmail.com>
2016-11-03 01:25:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ var submit=document.getElementById("commentSubmit");
 var comments=["Ok","wonderful","cool","good job"];
 var i=0;
 var list=" ";
-var m=document.getElementById("commentList");
+var m=document.getElementById("comment1");
 submit.onclick= function() {
     //make a request to the server
     //capture a list andrender it as a list
