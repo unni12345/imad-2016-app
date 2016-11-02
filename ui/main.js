03831654cfce2e288f233d1f66f8c51c15d5496f
unni12345 <rajkrishnanv@gmail.com>
2016-11-03 01:12:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ var comment=document.getElementById("comment").value;
 var submit=document.getElementById("commentSubmit");
 var comments=["Ok","wonderful","cool","good job"];
 var i=0;
-var list='';
+var list=" ";
 var m=document.getElementById("comment1");
 submit.onclick= function() {
     //make a request to the server
