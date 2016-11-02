ac0c49f4e4432c48263a91db46bb596913ed8881
unni12345 <rajkrishnanv@gmail.com>
2016-11-03 01:30:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,16 +4,19 @@ var comments=["Ok","wonderful","cool","good job"];
 var i=0;
 var list=" ";
 var m=document.getElementById("comment1");
+var n=document.getElementById("comment2");
+var o=document.getElementById("comment3");
 submit.onclick= function() {
     //make a request to the server
     //capture a list andrender it as a list
     //From now is shit
     
-      for(i;i<4;i++){
-          m.innerHTML=comments[i];
+          n.innerHTML=comments[1];
+          o.innerHTML=comments[2];
+          m.innerHTML=comments[0];
 
           
-      }
+      
       
 
     };
