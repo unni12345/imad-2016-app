c500ecbddaacb5fdfe99c96eae9dc1bd74abd31b
unni12345 <rajkrishnanv@gmail.com>
2016-11-03 01:53:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ var comment=document.getElementById("comment").value;
 var submit=document.getElementById("commentSubmit");
 var comments=["Ok","wonderful","cool","good job"];
 var i=0;
-var list=" ";
+var list="";
 var m=document.getElementById("commentList");
 var n=document.getElementById("comment2");
 var o=document.getElementById("comment3");
@@ -12,10 +12,10 @@ submit.onclick= function() {
     //From now is shit
     
           for(i=0;i<comments.length;i++){
-              m.innerHTML='<li>'+comments[i]+'</li>';
+           list='<li>'+comments[i]+'</li>';
           }
 
-          
+        m.innerHTML=list;  
       
       
 
