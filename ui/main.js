1b82d4a83cd98dbeebf4c4b650a5b34fbd047b5c
unni12345 <rajkrishnanv@gmail.com>
2016-11-03 16:08:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,15 +4,14 @@ var comments=["Ok","wonderful","cool","good job"];
 var i=0;
 var list = '';// the space is really important here , check for it always
 var m=document.getElementById("commentList");
-var n=document.getElementById("comment2");
-var o=document.getElementById("comment3");
+
 submit.onclick= function() {
     //make a request to the server
-    //capture a list andrender it as a list
+        //capture a list andrender it as a list
     //From now is shit
     
           for(i=0;i<4;i++){
-              list= list +'<li>'+comments[i]+'</li>';
+              list= list +'<li>'+comment+'</li>';
           
               
           }
