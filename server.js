9041e46299d7975af6a608f2345a837cbefc1b5c
unni12345 <rajkrishnanv@gmail.com>
2016-11-08 01:12:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ app.post('/create-user', function (req, res) {
    });
 });
 
-app.post('/login', function (req, res) {
+app.post('/sigin', function (req, res) {
    var username = req.body.username;
    var password = req.body.password;
    
