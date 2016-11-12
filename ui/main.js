c5a303f3a26ab7b9c951f6c9728e7348c5b933ec
unni12345 <rajkrishnanv@gmail.com>
2016-11-13 02:58:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,22 +6,29 @@ var i=0;
 var realComment=String(comment);
 submit.onclick= function() {
     //make a request to the server
-        //capture a list andrender it as a list
-    //From now is shit
-     m=document.getElementById("commentList");
-     var comment=document.getElementById("comment").value;
-     comments.push(comment);
-     m.innerHTML=m.innerHTML+'<li>'+comment+'</li>';
-     alert(comment);
+   
      var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-       // Typical action to be performed when the document is ready:
+     console.log("user loged in");
+     alert("logged in successfully");// Typical action to be performed when the document is ready:
     
+    }else if (this.status==403){
+        alert("username orpassword is incorrect");
+    }else (this.status==500){
+        alert("Something happened at the server");
+        
     }
 };
-xhttp.open("GET", "http://unni12345.imad.hasura-app.io/submit-comment?comment="+comment, true);
-xhttp.send();
+
+var username=document.getElementById("username").value;
+var password=document.getElementById("password").value;
+console.log("username");
+console.log("password");
+request.setRequestHeader('Content-Type','application/jason');
+
+xhttp.open("POST", "http://unni12345.imad.hasura-app.io/login", true);
+xhttp.send(JSON.stringify({username:username,password:password}));
  
     };
     /*       var request= new XMLHttpRequest();
